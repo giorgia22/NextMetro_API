88e81c0e33f4dc912d1b9b0d1b19833d26953beb
giorgia22 <g.tallino@libero.it>
2025-05-11 21:08:20 +0200

    Add parameters to getNextArrivals and add getStops function

--- main.py
+++ main.py
@@ -1,54 +1,76 @@
-from sqlalchemy import create_engine
+import uvicorn
+
+from sqlalchemy import create_engine, distinct
 from sqlalchemy.orm import Session
-from gtfsdb import StopTime, Trip, CalendarDate
+from gtfsdb import StopTime, Trip, UniversalCalendar, RouteStop
 import datetime
+import logging
 from fastapi import FastAPI
 
 app = FastAPI()
-DbUrl = "sqlite+pysqlite:///test_db.db"
+logger = logging.getLogger(__name__)
+
+# create with SQLAlchemy an engine to access the DB, with three / it's a local path
+DbUrl = "sqlite:///test_db.db"
 engine = create_engine(DbUrl)
 
 
-@app.get("/")
-def getNextArrivals():
-    # def getNextArrivals(stopId, fromDatetime, headsign="", count=1):
-    stopId = "VIMODRONE"
-    fromDatetime = datetime.datetime.now()
-    headsign = ""
-    count = 4
+@app.get("/next_arrivals/{stopId}/{date}")
+def getNextArrivals(
+    stopId: str, date: datetime.datetime, headsign: str = "", count: int = None
+):
+    logger.debug("getNextArrivals()")
 
+    # DB query with SQLAlchemy, it needs to open a session to the DB
     with Session(engine) as session:
         result = (
             session.query(StopTime)
-            .join(Trip, StopTime.trip_id == Trip.trip_id)
-            .join(CalendarDate, Trip.service_id == CalendarDate.service_id)
+            .join(Trip, StopTime.trip)
+            .join(UniversalCalendar, Trip.universal_calendar)
             .filter(
-                CalendarDate.date == fromDatetime.strftime("%Y-%m-%d"),
+                UniversalCalendar.date == date.astimezone().strftime("%Y-%m-%d"),
                 StopTime.stop_id == stopId,
-                StopTime.arrival_time > fromDatetime.strftime("%H:%M:%S"),
+                StopTime.arrival_time > date.astimezone().strftime("%H:%M:%S"),
             )
             .order_by(StopTime.arrival_time)
             .all()
         )
 
         t = 0
-        print("Next metro at Vimodrone for gessate: ")
-
         out = list(())
+
         for row in result:
             if headsign in row.stop_headsign:
                 out.append(row)
                 t += 1
-            if t >= count:
+            if count is not None and t >= count:
                 break
+
+        logger.info(
+            f"Get next {count} metro at {stopId} {'for' if headsign != '' else ''} {headsign}"
+        )
         return out
 
 
-# print(getNextArrivals("VIMODRONE", datetime.datetime.now(), "", count=4))
+@app.get("/stops")
+def getAllStops(metroLine: str = None):
+    logger.debug("getAllStops()")
+
+    with Session(engine) as session:
+        result = (
+            session.query(StopTime.stop_id, Trip.route_id)
+            .join(Trip, StopTime.trip)
+            .filter(Trip.route_id.like("M%" if metroLine is None else metroLine))
+            .distinct()
+        )
+    return result
+
 
 """
 TODO:
 - get nearest stop_id
 - search stop_ids
 - show stops map
+- api docs
+- better code
 """
